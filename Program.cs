db343972fd64c3cac1b389d5d0b52c8019c1a432
Бальтазар Лолронов <Slavacer@bk.ru>
2021-11-17 16:25:30 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using ASO_Docs;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace ASO_Test
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            WarrantRecord tst = new WarrantRecord();
+            tst.OpenDock(@"D:\Slava\Программы\Проекты\ASO_Docs\Testing.docx");
+            List<string> list = new List<string>{ "Проверка", "Связи" };
+            tst.SaveDock(tst.DoThings(list), @"D:\Slava\Testing.docx");
+        }
+    }
+}
